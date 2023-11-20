b8fb3acb5a16a3a55c8fd00c247754028d2fda50
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2023-11-20 18:06:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  // Remove the # prefix if present
+  shortHex = shortHex.replace(/^#/, '');
+
+  // Ensure that the shortHex is exactly 3 characters long
+  if (shortHex.length !== 3) {
+    return "Invalid short hex code. It should be 3 characters long.";
+  }
+
+  // Convert shortHex to full hex code by repeating each character
+  const fullHex = shortHex
+    .split('')
+    .map(char => char.repeat(2))
+    .join('');
+
+  // Add the # prefix and return the result
+  return `#${fullHex.toUpperCase()}`;
 };
 
 // Do not change the code below.
